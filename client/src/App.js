a8661cf6481d724c519dbb083e9dc627e047e7be
roy769 <roy@mortgallery.com>
2018-02-03 23:03:07 -0800

    material design bootstrap stylee

---
+++ client/src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+{/* 0 */}
+        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten">
+          <div className="row">
+            <div className="col">
+              <h1 className="logo animated bounce ">House Whores</h1>
+              <div className= "col">
+                <iframe className="loop" title="massive"></iframe>
+              </div>
+              <div className="col">
+                <a className="btn btn-success" data-toggle="collapsing" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+       <i class="material-icons">book</i>
+   </a>
+   <button className="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+       <i class="material-icons">store</i>
+   </button>
+ </div>
+ </div>
+</div>
+  </nav>
+{/*1*/}
+        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten-2">
+          <div className="row">
+            <div className="col">
+              <h1 className="logo animated flash ">Dj Devin</h1>
+              <div className= "col">
+                <iframe className="loop" title="developers"></iframe>
+              </div>
+              <div className="col">
+                <a className="btn btn-success" data-toggle="collapsing" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+       <i class="material-icons">book</i>
+   </a>
+   <button className="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+       <i class="material-icons">store</i>
+   </button>
+ </div>
+ </div>
+</div>
+  </nav>
+{/*1*/}
+        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten-3">
+          <div className="row">
+            <div className="col">
+              <h1 className="logo animated pulse ">Willie Funk</h1>
+              <div className= "col">
+                <iframe className="loop" title="sticky"></iframe>
+              </div>
+              <div className="col">
+                <a className="btn btn-success" data-toggle="collapsing" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+       <i class="material-icons">book</i>
+   </a>
+   <button className="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+       <i class="material-icons">store</i>
+   </button>
+ </div>
+ </div>
+</div>
+  </nav>
+        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten-4">
+          <div className="row">
+            <div className="col">
+              <h1 className="logo animated wobble ">Lino the Sound Guy</h1>
+              <div className= "col">
+                <iframe className="loop" title="lino"></iframe>
+              </div>
+              <div className="col">
+                <a className="btn btn-success" data-toggle="collapsing" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                <i class="material-icons"><i class="material-icons">book</i></i>
+   </a>
+   <button className="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+       <i class="material-icons">store</i>
+   </button>
+ </div>
+ </div>
+</div>
+  </nav>
+{/*1*/}
+      <footer>
+        <div className="footer-copyright">
+        <div className="container-fluid">
+            © 2018 Copyright: <a href="esreyconcepts.com"> esreyconcepts<img className="brand" src="https://farm5.staticflickr.com/4600/24162333177_5778c8d26f_t.jpg" width="78" height="100" alt="brand-image"></img> </a>
+
+        </div>
+    </div>
+
+</footer>
+
+
+
+
+  </div>
+
+    );
+  }
+}
+
+export default App;
