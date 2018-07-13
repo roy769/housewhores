71ed85aad10723388a3b77ca92d37837e649c718
roy769 <roy@mortgallery.com>
2018-07-12 22:06:10 -0700

    getting cleaner

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,19 @@ class App extends Component {
     return (
       <div className="App">
 {/* 0 */}
-        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten">
-          <div className="row">
-            <div className="col animated fadeIn"><img className="animated slideInLeft" src="https://media.giphy.com/media/pAjfVmRiGARfa/giphy.gif"></img>
+    <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten">
+      <div className="animated fadeIn">
+        <h1 className="logo animated bounce infinite ">House Whores</h1>
+      </div>
+    </nav>
+      <div className="container-fluid">
+          <div className="row blue-grey lighten-3">
+            <div className="col-xl-4 animated fadeIn"><img className="animated slideInLeft" src="https://media.giphy.com/media/pAjfVmRiGARfa/giphy.gif"></img>
           </div>
-            <div className="col animated fadeIn">
-              <h1 className="logo animated bounce infinite ">House Whores</h1>
-              <div className= "col animated fadeIn">
+              <div className= "col-xl-4 animated fadeIn">
                 <iframe className="loop" title="massive"></iframe>
               </div>
-              <div className="col animated fadeIn">
+              <div className="col-xl-4 animated fadeIn right">
                 <a className="btn animated rollIn btn-success" data-toggle="col animated fadeInlapsing" href="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
        <i class="material-icons">book</i>
    </a>
@@ -23,90 +26,27 @@ class App extends Component {
        <i class="material-icons">store</i>
    </button>
  </div>
- </div>
+
 </div>
-  </nav>
+</div>
 {/*1*/}
-        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten-2">
-          <div className="row">
-            <div className="col animated fadeIn">
-              <h1 className="logo animated flash infinite">Dj Devin</h1>
-              <div className= "col animated fadeIn">
-                <iframe className="loop" title="developers"></iframe>
-              </div>
-              <div className="col animated fadeIn">
-                <a className="btn animated rollIn btn-success" data-toggle="col animated fadeInlapsing" href="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
-       <i class="material-icons">book</i>
-   </a>
-   <button className="btn animated rollIn btn-success" type="button" data-toggle="col animated fadeInlapse" data-target="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
-       <i class="material-icons">store</i>
-   </button>
- </div>
-</div>
-<div className="col animated fadeIn"><img  className="animated slideInRight" src="https://media.giphy.com/media/pAjfVmRiGARfa/giphy.gif"></img>
-</div>
-</div>
-  </nav>
-{/*1*/}
-        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten-3">
-          <div className="row">
-            <div className="col animated fadeIn"><img  className=" animated slideInLeft" src="https://media.giphy.com/media/pAjfVmRiGARfa/giphy.gif"></img>
-          </div>
-            <div className="col animated fadeIn">
-              <h1 className="logo animated pulse infinite ">Willie Funk</h1>
-              <div className= "col animated fadeIn">
-                <iframe className="loop" title="sticky"></iframe>
-              </div>
-              <div className="col animated fadeIn">
-                <a className="btn animated rollIn btn-success" data-toggle="col animated fadeInlapsing" href="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
-       <i class="material-icons">book</i>
-   </a>
-   <button className="btn animated rollIn btn-success" type="button" data-toggle="col animated fadeInlapse" data-target="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
-       <i class="material-icons">store</i>
-   </button>
- </div>
- </div>
-</div>
-  </nav>
-        <nav className="navbar navbar-expand-sm navbar-light blue-grey lighten-4">
-          <div className="row">
-            <div className="col animated fadeIn animated fadeIn">
-              <h1 className="logo animated wobble  infinite ">Lino the Sound Guy</h1>
-              <div className= "col animated fadeIn">
-                <iframe className="loop" title="lino"></iframe>
-              </div>
-              <div className="col animated fadeIn">
-                <a className="btn animated rollIn btn-success" data-toggle="col animated fadeInlapsing" href="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
-                <i class="material-icons"><i class="material-icons">book</i></i>
-   </a>
-   <button className="btn animated rollIn btn-success" type="button" data-toggle="col animated fadeInlapse" data-target="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
-       <i class="material-icons">store</i>
-   </button>
-   <button className="btn animated rollIn btn-success" type="button" data-toggle="col animated fadeInlapse" data-target="#col animated fadeInlapseExample" aria-expanded="false" aria-controls="col animated fadeInlapseExample">
-       <i class="material-icons">face</i>
-       <i class="material-icons">book</i>
-   </button>
- </div>
- </div>
- <div className="col animated fadeIn"><img  className="animated slideInRight" src="https://media.giphy.com/media/pAjfVmRiGARfa/giphy.gif"></img>
-</div>
-</div>
-  </nav>
-{/*1*/}
+      <div className="row align-wrapper">
+        <div className="col animated flash infinite">
+        <h1 className="logo animated fadeIn infinite center">DJ Devin</h1>
+      </div>
+      </div>
       <footer>
         <div className="footer-copyright">
         <div className="container-fluid">
-            © 2018 Copyright: <a href="esreyconcepts.com"> esreyconcepts<img className="brand" src="https://farm5.staticflickr.com/4600/24162333177_5778c8d26f_t.jpg" width="78" height="100" alt="brand-image"></img> </a>
+            © 2018 Copyright: <a href="esreyconcepts.com"> esreyconcepts<img className="brand" src="https://farm5.staticflickr.com/4600/24162333177_5778c8d26f_t.jpg" width="78" height="100" alt="brand-image"></img></a>
 
         </div>
     </div>
-
 </footer>
 
+</div>
 
 
-
-  </div>
 
     );
   }
